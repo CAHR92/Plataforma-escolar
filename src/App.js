fdd7e7386772045a29892abf8d64fd09ac39c7b5
Cesar Hoil <cesarhoil_92@hotmail.com>
2022-04-11 13:52:01 -0500

    Agregando Index y App . js

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route, Link
+} from "react-router-dom";
+import { About } from './components/About';
+import { Login } from './components/Login';
+import { Students } from './components/Students';
+
+
+
+export const App = () => {
+  
+
+  return (
+    <Router>
+      <div className='nav'>
+        <nav className='navigation'>
+            <Link className='link' to='/student' element={<Students />}>Alumnos</Link>
+            <Link className='link' to='/about' element={<About />}>About</Link>
+            <Link className='link' to='/login' element={<Login />}>Login</Link>
+            
+        </nav>
+      </div>
+      <div>
+        <Routes>
+            <Route path='/students' element={<Students />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<Students/>} />
+
+          </Routes>
+      </div>
+    </Router>
+    
+     
+  )
+}
